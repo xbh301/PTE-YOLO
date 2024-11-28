4f1f1f0a4c35209ffed4a63164dd9c419d4b7ebd
bohan301 <127025791+bohan301@users.noreply.github.com>
2024-11-28 20:23:21 +0800

    Update input.py

--- models/AAA/input.py
+++ models/AAA/input.py
@@ -9,14 +9,3 @@ class input(nn.Module):
 
     def forward(self, x):
         return x
-
-
-class b_v22_1_input(nn.Module):
-    def __init__(self):
-        super(b_v22_1_input, self).__init__()
-
-    def forward(self, x):
-        return x[0]
-
-
-
