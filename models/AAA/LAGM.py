d9b6d070203a97d42e9921cc984b4cd8deff775f
xbh301 <127025791+xbh301@users.noreply.github.com>
2025-03-31 16:32:23 +0800

    Update LAGM.py

--- models/AAA/LAGM.py
+++ models/AAA/LAGM.py
@@ -4,54 +4,38 @@ import torch.nn.functional as F
 import random
 
 
-class v35_LAGM(nn.Module):         # Light Aware Guidance Module
-    def __init__(self):
+class v63_PG(nn.Module):         # Light Aware Guidance Module
+    def __init__(self, in_channels, mid_channels):
         super().__init__()
         self.cbl1 = nn.Sequential(
-            nn.Conv2d(15, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(inplace=True))
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(mid_channels),
+            nn.LeakyReLU(inplace=True)
+        )
         self.cbl2 = nn.Sequential(
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(mid_channels, mid_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(mid_channels),
             nn.LeakyReLU(inplace=True)
         )
         self.cbl3 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(mid_channels, mid_channels*2, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(mid_channels*2),
             nn.LeakyReLU(inplace=True)
         )
         self.cbl4 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(inplace=True),
-            nn.AdaptiveAvgPool2d(1)
+            nn.Conv2d(mid_channels*2, mid_channels*4, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(mid_channels*4),
+            nn.LeakyReLU(inplace=True)
         )
         self.downsample = nn.AvgPool2d(2, 2)
-        self.fc = nn.Sequential(
-            nn.Linear(128, 128),
-            nn.LeakyReLU(inplace=True),
-            nn.Linear(128, 128)                    
 
     def forward(self, x):
         img_patches = x[1]
         unbound = torch.unbind(img_patches, dim=1)
         img_patches = torch.cat(unbound, dim=1)
-        x = self.downsample(self.cbl1(img_patches))
-        x = self.downsample(self.cbl2(x))
-        x = self.cbl4(self.cbl3(x))
-        fea = x.view(x.size(0), -1) 
-        return fea
 
-class v27_3_fc_layer(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc = nn.Sequential(
-            nn.Linear(128, 128),
-            nn.LeakyReLU(inplace=True),
-            nn.Linear(128, 97)
-        )
+        prompt_en = self.cbl2(self.cbl1(img_patches))
+        prompt_layer1 = self.cbl3(self.downsample(prompt_en))
+        prompt_layer2 = self.cbl4(self.downsample(prompt_layer1))
 
-    def forward(self, x):
-        x = self.fc(x)
-        return x
+        return [prompt_en, prompt_layer1, prompt_layer2]
