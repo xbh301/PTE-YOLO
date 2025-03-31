738f8adfcb9145d25a8f7b740bb7749ee0d128f7
xbh301 <127025791+xbh301@users.noreply.github.com>
2025-03-31 16:29:32 +0800

    Update filter.py

--- models/AAA/filter.py
+++ models/AAA/filter.py
@@ -67,46 +67,54 @@ class Filter:
     return low_res_output
 
 
-class ReciprocalCurveFilter(Filter):
+class v60_ReciprocalCurveFilter(Filter):
   def __init__(self, cfg):
     super().__init__(cfg)
     self.begin_filter_parameter = cfg.reciprocal_curve_begin_param
-    self.num_filter_parameters = 1
+    self.num_filter_parameters = 3
 
   def filter_param_regressor(self, features):
     log_alpha_left = np.log(self.cfg.alpha_range[0])
     log_alpha_right = np.log(self.cfg.alpha_range[1])
     log_alpha = tanh_range(log_alpha_left, log_alpha_right)(features)
-    alpha = torch.exp(log_alpha) 
+    alpha = torch.exp(log_alpha)  
     return alpha
 
   def process(self, img, param):
     # param = param+0.2
-    alpha = param.repeat([1, 3])
-    alpha = alpha[:, :, None, None]
+    alpha = param[:, :, None, None]
     enhanced_img = (alpha + 1) * img / (img + alpha)
     return enhanced_img
 
 
-class v19_DIP(nn.Module):
+class v60_DIP(nn.Module):
   def __init__(self, cfg):
     super().__init__()
-    self.reciprocal_curve = ReciprocalCurveFilter(cfg)
+    self.reciprocal_curve = v60_ReciprocalCurveFilter(cfg)
 
   def forward(self, img, param):
     img = self.reciprocal_curve.apply(img, param)
     return img
 
 
-class v19_ImageLevelEnhancement(nn.Module):
+class v60_ImageLevelEnhancement(nn.Module):
   def __init__(self):
     super().__init__()
-    self.num_filter_parameters = 1
+    self.num_filter_parameters = 3
     self.reciprocal_curve_begin_param = 0
-    self.alpha_range = (0.01, 10.0)
-    self.dip = v19_DIP(self)
+    self.alpha_range = (0.001, 100.0)
+    self.GAP = nn.AdaptiveAvgPool2d(1)
+    self.fc = nn.Sequential(
+      nn.Linear(16, 16),
+      nn.LeakyReLU(inplace=True),
+      nn.Linear(16, 3) 
+    )
+
+    self.dip = v60_DIP(self)
 
   def forward(self, x):
-    param = x[0]              
+    param = x[0][0]              
+    low = x[1][0]
+    param = self.fc(self.GAP(param).view(low.size(0), -1))
     en = self.dip(low, param).type_as(low)
     return en
